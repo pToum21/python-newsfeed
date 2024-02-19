d3a6d6f17f4e4efb0f0c779040796b856ab5724a
toum21 <pey2moo1@gmail.com>
2024-02-18 18:00:21 -0800

    began to work on setting up flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
 
 
 def create_app(test_config=None):
@@ -15,4 +16,5 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  init_db()
   return app
